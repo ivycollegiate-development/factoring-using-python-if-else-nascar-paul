226b556a7ce53e7ec48988a63a1016e8c43e6dd7
Paul Jones <paul.tyler.jones@gmail.com>
2024-10-04 08:14:17 +0000

    Comments my answer

--- MrJones_Sprint_Projectv2.75/MrJones_Sprint_Projectv2.76.py
+++ MrJones_Sprint_Projectv2.75/MrJones_Sprint_Projectv2.76.py
@@ -1,3 +1,5 @@
 # List all of the factors of a value
 def list_factors(n):
+    # Explain the line of code below
     return [i for i in range (1, n+1) if n % i == 0]
+    # Comment your explination here
